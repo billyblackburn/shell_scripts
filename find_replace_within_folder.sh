b7bd56d496aa2e016233ee83bcbce46128384d65
billy <blackburnwilliam2@gmail.com>
2023-03-15 00:19:51 -0400

    Update find_replace_within_folder.sh

--- find_replace_within_folder.sh
+++ find_replace_within_folder.sh
@@ -1,2 +1,4 @@
 find . -type f -exec sed -i 's+CUDA_PATH ?= /usr/local/cuda+CUDA_PATH ?= /usr+g' {} +
 # this will work on strings that have "/" slashes in them
+# I used it to edit hundreds of cuda Makefile that were incorrect 
+# due to PopOS not being congruent with Ubuntu when the compiler searches for Distro
